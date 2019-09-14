1caaa5c50b34733d6438af3951fdcc71f7520fc4
phucvo27 <learninge27@gmail.com>
2019-09-15 00:57:03 +0700

    fixed some logic

--- controllers/chatControllers.js
+++ controllers/chatControllers.js
@@ -1,17 +1,20 @@
 const { Chat } = require('../models/Chat');
 
 exports.getChat = async (req, res, next)=>{
-    req.user = "5d7c9d174cbaa11bbe0a37a5";
+    //req.user = "5d7d044d0ba3340d72f5a55c";
     const { chatWith } = req.params;
     let roomName = '';
     if(chatWith){
-
+        req.user = "5d7d044d0ba3340d72f5a55c" === chatWith ? "5d7d044d0ba3340d72f5a55d" : "5d7d044d0ba3340d72f5a55c"
         if(req.user < chatWith){
+            console.log("in req.user < chatWith")
             roomName = `${req.user}-${chatWith}`
         }else{
             roomName = `${chatWith}-${req.user}`
         }
+        console.log(roomName)
         const chat = await Chat.findOne({room: roomName}).populate('messages');
+        console.log(chat)
         res.status(200).send({
             chat
         })
